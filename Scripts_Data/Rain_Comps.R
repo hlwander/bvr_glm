71883f574c727299c1915d1b454300c98f035cb0
Heather Wander <hwander@vt.edugit config --global user.name Heather>
2022-12-05 09:22:39 -0500

    comparing nldas vs met data

--- Scripts_Data/Rain_Comps.R
+++ Scripts_Data/Rain_Comps.R
@@ -4,13 +4,11 @@
 # Load libraries
 pacman::p_load(tidyverse,ggplot2,zoo)
 
-setwd("C:/Users/ahoun/OneDrive/Desktop/BVR-GLM/BVR-GLM/Raw_Data")
-
 # Load data: USGS Roanoke Gage (from Heather); downloaded NLDAS data; FCR precip (from EDI)
-usgs <- read_csv("USGS_precip_2013-2019.csv")
+usgs <- read_csv("./Raw_Data/USGS_precip_2013-2019.csv")
 usgs$mdate <- as.POSIXct(strptime(usgs$mdate, "%m/%d/%Y", tz = "EST"))
 
-nldas <- read_csv('C:/Users/ahoun/OneDrive/Desktop/BVR-GLM/BVR-GLM/inputs/BVR_GLM_NLDAS_010113_123119_GMTadjusted.csv')
+nldas <- read_csv('./inputs/BVR_GLM_NLDAS_010113_123119_GMTadjusted.csv')
 nldas$time <- as.POSIXct(strptime(nldas$time, "%Y-%m-%d", tz = "EST"))
 
 inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/389/4/c1db8816742823eba86696b29f106d0f" 
@@ -25,9 +23,29 @@ fcr$DateTime <- as.POSIXct(strptime(fcr$DateTime, "%Y-%m-%d", tz = "EST"))
 nldas_rain <- nldas %>% select(time,Rain) %>% mutate(Rain_hr = Rain*1000/24) %>% 
   group_by(time) %>% summarise_all(funs(sum))
 
+nldas_avg <- nldas %>% select(time,AirTemp,ShortWave,LongWave) %>%
+  group_by(time) %>% summarise_all(funs(mean))
 
 # Average FCR data across days (mm/d)
 fcr_rain <- fcr %>% select(DateTime,Rain_Total_mm) %>% group_by(DateTime) %>% summarise_all(funs(sum))
+fcr_avg <- fcr %>% select(DateTime, AirTemp_Average_C,ShortwaveRadiationUp_Average_W_m2,InfaredRadiationUp_Average_W_m2) %>%
+                   group_by(DateTime) %>% summarise_all(funs(mean))
+
+#NLDAS vs fcr met
+nldas_rain_sub <- nldas_rain$Rain[match(fcr_rain$DateTime,nldas_rain$time)]
+nldas_airtemp <- nldas_avg$AirTemp[match(fcr_avg$DateTime,nldas_avg$time)]
+nldas_shortwave <- nldas_avg$ShortWave[match(fcr_avg$DateTime,nldas_avg$time)]
+nldas_longwave <- nldas_avg$LongWave[match(fcr_avg$DateTime,nldas_avg$time)]
+
+plot(nldas_rain_sub, fcr_rain$Rain_Total_mm)
+plot(nldas_airtemp, fcr_avg$AirTemp_Average_C)
+plot(nldas_shortwave, fcr_avg$ShortwaveRadiationUp_Average_W_m2)
+plot(nldas_longwave, fcr_avg$InfaredRadiationUp_Average_W_m2)
+
+cor.test(nldas_rain_sub,fcr_rain$Rain_Total_mm)
+cor.test(nldas_airtemp, fcr_avg$AirTemp_Average_C)
+cor.test(nldas_shortwave, fcr_avg$ShortwaveRadiationUp_Average_W_m2)
+cor.test(nldas_longwave, fcr_avg$InfaredRadiationUp_Average_W_m2)
 
 # Plot them all
 ggplot()+
